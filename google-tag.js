12ffbf7b2aa66d7a5f788ba5ae7b83c283cd7856
Hugo Moreira <hugo.moreira@visma.com>
2026-01-19 17:42:00 +0000

    add tag to page visualization

--- google-tag.js
+++ google-tag.js
@@ -1,11 +1,5 @@
 // Google tag (gtag.js)
 (function() {
-  // Load the gtag.js script
-  var script = document.createElement('script');
-  script.async = true;
-  script.src = 'https://www.googletagmanager.com/gtag/js?id=AW-17888254668';
-  document.head.appendChild(script);
-
   // Initialize dataLayer and gtag function
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
@@ -14,4 +8,21 @@
   // Configure gtag
   gtag('js', new Date());
   gtag('config', 'AW-17888254668');
+
+  // Load the gtag.js script
+  var script = document.createElement('script');
+  script.async = true;
+  script.src = 'https://www.googletagmanager.com/gtag/js?id=AW-17888254668';
+  
+  // Fire conversion event after script loads
+  script.onload = function() {
+    // Event snippet for Visualização de página conversion page
+    gtag('event', 'conversion', {
+      'send_to': 'AW-17888254668/pnSfCO7-xegbEMy15NFC',
+      'value': 1.0,
+      'currency': 'EUR'
+    });
+  };
+  
+  document.head.appendChild(script);
 })();
